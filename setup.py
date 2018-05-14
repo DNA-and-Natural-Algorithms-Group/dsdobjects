5975600a700f02d4fcfcb2b091427d7fb1a64357
Stefan Badelt <badelt@caltech.edu>
2018-05-14 15:35:47 -0700

    added download url

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     author='Stefan Badelt',
     author_email='badelt@caltech.edu',
     license=license,
+    download_url = 'https://github.com/DNA-and-Natural-Algorithms-Group/dsdobjects/archive/v0.6.tar.gz',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2.7',
